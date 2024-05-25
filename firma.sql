be26683be22c17c7cafb2711c92b480b57c8f1fb
Marko Virovac <virovac81@gmail.com>
2024-05-25 12:27:53 +0200

    primjeri

---
+++ firma.sql
@@ -0,0 +1,10 @@
+--create database firma;
+
+--use firma;
+
+--create table radnik(
+--sifra int,
+--ime nvarchar(50),
+--prezime nvarchar(50),
+--nadredeni int
+--);
---
+++ polaznik.sql
@@ -0,0 +1,31 @@
+--create database odjel;
+
+--use  odjel;
+
+--create table zaposlenik(
+--sifra int,
+--nadredeni int,
+--ime varchar(45),
+--prezime varchar(45),
+--datumrodenja datetime,
+--placa decimal(18,2),
+--odjel int
+--);
+
+--create table projekt(
+--sifra int,
+--naziv varchar(50),
+--datumpocetka datetime,
+--datumkraja datetime
+--);
+
+--create table odjel(
+--sifra int,
+--naziv varchar(45)
+--);
+
+--create table sudjeluje(
+--zaposlenik int,
+--projekt int,
+--datimpristupanja datetime
+--);
