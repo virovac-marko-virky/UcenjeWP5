04d284aef6b850fd2475266c7e4fb3d0e7f10fbd
virovac-marko-virky <virovac81@gmail.com>
2024-06-15 08:56:10 +0200

    odrađene dvije vježbe

---
+++ vjezba skripta 3.sql
@@ -0,0 +1,27 @@
+﻿use master;
+go
+
+drop database if exists samostan;
+go
+
+create database samostan collate Croatian_CI_AS;
+go
+
+use samostan;
+
+create table svecenici(
+sifra int not null primary key identity(1,1),
+ime varchar(50) not null,
+prezime varchar(50) not null,
+nadređeni varchar(50) not null
+);
+
+select * from svecenici;
+
+insert into svecenici(ime,prezime,nadređeni)
+values ('Ante', 'Guberina', 'Don Viktor');
+insert into svecenici(ime,prezime,nadređeni)
+values ('Mirko', 'Vulina', 'Don Gabrijel');
+insert into svecenici(ime,prezime,nadređeni)
+values ('Valent', 'Skender', 'Don Martin');
+
